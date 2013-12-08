e59a43946e0be4254e04ac89aadb764d084e384c
Colinj5 <colinj5@vt.edu>
2013-12-08 01:47:00 -0500

    Added methods in Encryption and Decryption

--- RSAcrypt.cpp
+++ RSAcrypt.cpp
@@ -13,8 +13,13 @@
  */
 
 #include <string>
+#include <sstream>
 #include "RSAcrypt.h"
 #include "B64coder.h"
+#include <gmp.h>
+
+//temp
+using namespace std;
 
 class RSAdata
 {
@@ -43,12 +48,56 @@ RSAcrypt::~RSAcrypt()
 
 char* RSAcrypt::encrypt(char* data)
 {
+	string key = RSA->pubKey;
+	char *D;
+	unsigned long int exp, temp;
+	istringstream ss(key);
+	mpz_t base;
+	mpz_init(base);
+	mpz_t mod;
+ 	mpz_t rop;
+	mpz_init(rop);
+	ss>>exp>>temp;
+	mpz_init_set_ui(mod, temp);
+	for(unsigned int x = 0; x < (16/sizeof(char)); x++)
+	{
+		*D = data[x];
+		mpz_set_str( base, D, 10);
+		mpz_powm_ui( rop, base, exp, mod);
+		D = mpz_get_str( D, 10, rop);
+		data[x] = *D; 
+	}
+	mpz_clear(base);
+	mpz_clear(rop);
+	mpz_clear(mod);	
 	return data;
 }
 
 char* RSAcrypt::decrypt(char* data)
 {
-	return data;
+	string key = RSA->prvKey;
+        char *D;
+        unsigned long int exp, temp;
+        istringstream ss(key);
+        mpz_t base;
+        mpz_init(base);
+        mpz_t mod;
+        mpz_t rop;
+        mpz_init(rop);
+        ss>>exp>>temp;
+        mpz_init_set_ui(mod, temp);
+        for(unsigned int x = 0; x < (16/sizeof(char)); x++)
+        {
+                *D = data[x];
+                mpz_set_str( base, D, 10);
+                mpz_powm_ui( rop, base, exp, mod);
+                D = mpz_get_str( D, 10, rop);
+                data[x] = *D;
+        }
+        mpz_clear(base);
+        mpz_clear(rop);
+        mpz_clear(mod);
+        return data;
 }
 
 void RSAcrypt::setKeys(const char* pubKey, const char* prvKey)
@@ -59,8 +108,8 @@ void RSAcrypt::setKeys(const char* pubKey, const char* prvKey)
 
 void RSAcrypt::genKeys()
 {
-	RSA->pubKey = "NewPubKey";
-	RSA->prvKey = "NewPrvKey";	
+	RSA->pubKey = "NewPub Key";
+	RSA->prvKey = "NewPrv Key";	
 }	
 	
 const char* RSAcrypt::getPubKey()
